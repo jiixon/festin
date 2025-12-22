cdb07a7000ad2ce7c0b88acfbb88af8ae81cdf43
jiixon <jr0107380@gmail.com>
2025-12-23 05:10:31 +0900

    refactor: 로그 추가

--- src/test/java/com/festin/app/config/TestcontainersConfiguration.java
+++ src/test/java/com/festin/app/config/TestcontainersConfiguration.java
@@ -18,23 +18,33 @@ public class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:8.0"));
+        MySQLContainer<?> container = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+                .withReuse(false);
+        System.out.println("=== MySQL Container Starting ===");
+        return container;
     }
 
     @Bean
     @ServiceConnection(name = "redis")
     org.testcontainers.containers.GenericContainer<?> redisContainer() {
-        return new org.testcontainers.containers.GenericContainer<>(DockerImageName.parse("redis:7.0-alpine"))
+        org.testcontainers.containers.GenericContainer<?> container =
+            new org.testcontainers.containers.GenericContainer<>(DockerImageName.parse("redis:7.0-alpine"))
                 .withExposedPorts(6379)
+                .withReuse(false)
                 .waitingFor(Wait.forLogMessage(".*Ready to accept connections.*\\n", 1))
                 .withStartupTimeout(Duration.ofMinutes(2));
+        System.out.println("=== Redis Container Starting ===");
+        return container;
     }
 
     @Bean
     @ServiceConnection
     RabbitMQContainer rabbitMQContainer() {
-        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:3-management-alpine"))
-                .waitingFor(Wait.forListeningPort())  // 포트 열릴 때까지 대기
-                .withStartupTimeout(Duration.ofMinutes(2));  // 타임아웃 여유있게
+        RabbitMQContainer container = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3-management-alpine"))
+                .withReuse(false)
+                .waitingFor(Wait.forListeningPort())
+                .withStartupTimeout(Duration.ofMinutes(2));
+        System.out.println("=== RabbitMQ Container Starting ===");
+        return container;
     }
 }
